27570dd01c9732c98740b0d16139e7ec6ea837a1
KyleChutjian <kyle.chutjian@gmail.com>
2021-02-03 14:33:29 -0500

    added bye Alex

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi Alex");
         System.out.println("Hello Alex");
-
+        System.out.println("Bye Alex");
     }
 
 }
